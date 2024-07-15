4b6d44a09fc8db7ccd8a5786a19f6e96297285db
athena272 <guilhermera272@gmail.com>
2024-07-15 11:05:48 -0300

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,12 @@
+import Link from "next/link"
+
 export default function FAQPage() {
     return (
         <div>
             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
-            <a href="/">Ir para a home</a>
+            <Link href="/">
+                <a >Ir para a home</a>
+            </Link>
         </div>
     )
 }
