a54942a222e8e850e76fc8d34eac5176a66a368e
athena272 <guilhermera272@gmail.com>
2024-07-15 23:20:21 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function HomePage() {
     return (
         <div>
             <h1>Alura Cases - Home Page</h1>
-            <Link href="/faq">
+            <Link href="/faq" passHref>
                 <a>Ir para o FAQ</a>
             </Link>
         </div>
