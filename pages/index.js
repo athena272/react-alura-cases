7c79ac6344af62fb0c45b27d6cd7876ec4cd1626
athena272 <guilhermera272@gmail.com>
2024-07-20 16:36:32 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import Link from "../src/components/Link"
 
+function Title({ children, as }) {
+    const Tag = as
+
+    return (
+        <>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: red;
+                }
+            `}</style>
+        </>
+    )
+}
+
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home Page</h1>
+            <Title as={'h1'}>Alura Cases - Home Page</Title>
             <Link
                 href={'/faq'}
             >
