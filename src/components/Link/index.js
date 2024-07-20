497e50e85e0cdd5718ca7d50765c2b5ab7c215f1
athena272 <guilhermera272@gmail.com>
2024-07-20 00:12:54 -0300

    Update index.js

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,9 +1,12 @@
 import NextLink from "next/link";
+import LinkEstilizado from "../LinkEstilizado";
 
 export default function Link({ href, children, ...props }) {
     return (
         <NextLink href={href} passHref>
-            <a {...props}>{children}</a>
+            <LinkEstilizado {...props}>
+                {children}
+            </LinkEstilizado>
         </NextLink>
     )
 } 
