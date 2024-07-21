e27e036006f2341944dc5f030078fb1bfcc6aca5
athena272 <guilhermera272@gmail.com>
2024-07-20 22:52:20 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,19 @@ export default function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <script 
+                {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+                {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-2QZDPX3BLK"></script>
+                <script
                     dangerouslySetInnerHTML={{
                         __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-2QZDPX3BLK');
                         `
                     }}
-                />
+                /> */}
 
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
